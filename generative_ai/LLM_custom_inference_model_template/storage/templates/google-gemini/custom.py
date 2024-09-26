df9a57ca3d0657b031239ff7385a781e2a3b6720
Luke Shulman <lshulman@gmail.com>
2024-09-26 19:27:31 -0400

    fix token expirations

--- generative_ai/LLM_custom_inference_model_template/storage/templates/google-gemini/custom.py
+++ generative_ai/LLM_custom_inference_model_template/storage/templates/google-gemini/custom.py
@@ -9,10 +9,16 @@ import openai
 from openai import OpenAI
 from openai.types.chat import ChatCompletion, ChatCompletionChunk, CompletionCreateParams
 import google.auth.transport.requests
+from google.oauth2 import service_account
 import requests
+from datarobot_drum.drum import description
+from time import time
+
+print(f"The DRUM Version is {description.version}")
 
 
 LOCATION = 'us-central1'
+TOKEN_CREATE_TIME = time()
 
 def get_google_credential():
     if os.getenv(
@@ -23,9 +29,10 @@ def get_google_credential():
             "GOOGLE_APPLICATION_RUNTIME_CREDENTIAL"
         )
         key = supplied_credential["gcpKey"]
-        credential, project = google.auth.load_credentials_from_dict(key)
+        raw_credential, project = google.auth.load_credentials_from_dict(key)
+        credential = raw_credential.with_scopes(
+            ['https://www.googleapis.com/auth/cloud-platform'])
     else:
-        # try to use default credentials
         try:
             credential, project = google.auth.default()
             print(f"Using Default Credentials for project {project}")
@@ -36,22 +43,33 @@ def get_google_credential():
             )
     return credential, project
 
+def get_token(credential):
+    global TOKEN_CREATE_TIME
+    TOKEN_CREATE_TIME = time()
+    auth_req = google.auth.transport.requests.Request()
+    credential.refresh(auth_req)
+    return credential.token
 
 
 
 def load_model(*args, **kwargs):
+    print(f"The DRUM Version is {description.version}")
     credential, project = get_google_credential()
-    auth_req = google.auth.transport.requests.Request()
-    credential.refresh(auth_req)
+    print(f"Got the Credential for {project}")
+    token = get_token(credential)
     client = openai.OpenAI(
     base_url = f'https://{LOCATION}-aiplatform.googleapis.com/v1beta1/projects/{project}/locations/{LOCATION}/endpoints/openapi',
-    api_key = credential.token)
-    return client
+    api_key = token)
+    return credential, client
 
 
 def score(data, model, **kwargs):
-    prompts = data["promptText"].tolist()
+    prompts = data["prompt"].tolist()
     responses = []
+    credential, client = model
+    if (time() - TOKEN_CREATE_TIME) > 1800:
+        token = get_token(credential)
+        client.api_key = token
     for p in prompts:
         completion_create_params = {
         "model": "google/gemini-1.5-flash-001",
@@ -62,18 +80,18 @@ def score(data, model, **kwargs):
         },
         {
             "role": "user",
-            "content": "Hello!"
+            "content": p
         }
         ]
     }
-        response = model.chat.completions.create(**completion_create_params)
+        response = client.chat.completions.create(**completion_create_params)
         response_text = response.choices[0].message.content
         responses.append(response_text)
 
-    return pd.DataFrame({"resultText": responses})
+    return pd.DataFrame({"responseText": responses})
 
 
-def chat(completion_create_params: CompletionCreateParams, model: OpenAI)-> ChatCompletion:
+def chat(completion_create_params: CompletionCreateParams, model: OpenAI, **kwargs)-> ChatCompletion:
     """Chat Hook compatibale with ChatCompletion
     OpenAI Specification
 
@@ -89,20 +107,24 @@ def chat(completion_create_params: CompletionCreateParams, model: OpenAI)-> Chat
     ChatCompletion
         the completion object with generated choices. 
     """
-    return model.chat.completions.create(**completion_create_params)
+    credential, client = model
+    if (time() - TOKEN_CREATE_TIME) > 1800:
+        token = get_token(credential)
+        client.api_key = token
+    return client.chat.completions.create(**completion_create_params)
 
-m = load_model()
-chat({
-    "model": "google/gemini-1.5-flash-001",
-    "messages": [
-      {
-        "role": "system",
-        "content": "You are a helpful assistant."
-      },
-      {
-        "role": "user",
-        "content": "Hello!"
-      }
-    ]
-  }, m)
-score(pd.DataFrame([{'promptText': "Hi who are you"}]), m)
+# m = load_model()
+# chat({
+#     "model": "google/gemini-1.5-flash-001",
+#     "messages": [
+#       {
+#         "role": "system",
+#         "content": "You are a helpful assistant."
+#       },
+#       {
+#         "role": "user",
+#         "content": "Hello!"
+#       }
+#     ]
+#   }, m)
+# score(pd.DataFrame([{'promptText': "Hi who are you"}]), m)
