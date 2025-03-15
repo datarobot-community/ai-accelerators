8dd9ef46a80e0d1c65dc71321f6db5a01add1834
Lukas Innig <lukas.innig@datarobot.com>
2025-03-15 18:13:41 +0000

    switch to litellm

--- generative_ai/LLM_custom_inference_model_template/storage/templates/anthropic-claude/custom.py
+++ generative_ai/LLM_custom_inference_model_template/storage/templates/anthropic-claude/custom.py
@@ -1,11 +1,27 @@
-import os
-from time import time
+# Copyright 2024 DataRobot, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from anthropic import Anthropic
-from anthropic.types import CompletionCreateParams, Message
+import os
+from typing import Any, Iterator, Union
 from datarobot_drum import RuntimeParameters
-from openai.types.chat import ChatCompletion
 import pandas as pd
+from litellm import completion
+from openai.types.chat import (
+    ChatCompletion,
+    ChatCompletionChunk,
+    CompletionCreateParams,
+)
 
 
 def load_model(*args, **kwargs):
@@ -13,67 +29,34 @@ def load_model(*args, **kwargs):
         api_key = env_key
     else:
         api_key = RuntimeParameters.get("ANTHROPIC_API_KEY")["apiToken"]
-    return Anthropic(api_key=api_key)
+        os.environ["ANTHROPIC_API_KEY"] = api_key
+    return "done"
 
 
-def score(data: pd.DataFrame, model: Anthropic, **kwargs):
+# azure call
+
+
+def score(data: pd.DataFrame, model: str, **kwargs: Any) -> pd.DataFrame:
     prompts = data["promptText"].tolist()
     responses = []
 
-    for prompt in prompts:
-        response = model.messages.create(
-            max_tokens=1024,
-            temperature=0.5,
-            top_p=1.0,
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                }
+    for p in prompts:
+        completion_create_params = {
+            "model": "claude-3-sonnet-20240229",
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": p},
             ],
-            model="claude-3-sonnet-20240229",
-        )
-        responses.append(response.content[0].text)
+        }
+        response = completion(**completion_create_params)
+        response_text = response.choices[0].message.content
+        responses.append(response_text)
 
     return pd.DataFrame({"resultText": responses})
 
 
-def chat(completion_create_params: CompletionCreateParams, model: Anthropic) -> Message:
-    """Chat Hook compatibale with ChatCompletion
-    OpenAI Specification
-
-    Parameters
-    ----------
-    completion_create_params : CompletionCreateParams
-        object that holds all the parameters needed to create the chat completion.
-    model : Anthropic
-        The model object from openai.  this is injected output from load_model
-
-    Returns
-    -------
-    ChatCompletion
-        the completion object with generated choices.
-    """
-    an_message: Message = model.messages.create(**completion_create_params)
-    msg = an_message.content[0]  # TODO: Handle multiple content blocks
-
-    finish_reason = {
-        "end_turn": "stop",
-        "stop_sequence": "stop",
-        "max_tokens": "length",
-        "tool_use": "tool_calls",
-    }[an_message.stop_reason]
-
-    return ChatCompletion(
-        id=an_message.id,
-        choices=[
-            {
-                "message": {"role": an_message.role, "content": msg.text},
-                "finish_reason": finish_reason,
-                "index": 0,
-            }
-        ],
-        model=str(an_message.model),
-        created=int(time()),
-        object="chat.completion",
-    )
+def chat(
+    completion_params: CompletionCreateParams,
+    model: str,
+) -> Union[ChatCompletion, Iterator[ChatCompletionChunk]]:
+    return completion(**completion_params)
